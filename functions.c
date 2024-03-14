2276d09bb3bf49ad1c5eeebf4ad367d69b468658
Hursh Gupta <hurshgupta1@gmail.com>
2024-03-14 15:54:48 +0530

    removes all non-necessary formatting chars

--- functions.c
+++ functions.c
@@ -1,21 +1,21 @@
 
 #include "main.h"
 
-void append(String S, char c);
+void attach(String S, char c);
 int isFormatChar(char c);
 
 String remove_existing_formatting(char* buffer, long length){
 
   String S = (String)malloc(sizeof(struct strNode)); // Dummy node
   int INSTRING = 0; // am I inside a string literal -1: No, +1: Yes
-  Node t = S;
+  Node p, q, t = S;
   t->next = NULL;
 
   for(int i = 0; i < length; i++ ){
 
     switch(buffer[i]){
       case '"':
-        append(t, buffer[i]);
+        attach(t, buffer[i]);
         t = t->next;
         if (INSTRING)
           INSTRING = 0; // switch INSTRING
@@ -26,13 +26,15 @@ String remove_existing_formatting(char* buffer, long length){
       case '}':
       case '{':
       case ';':
+      case '=':
       case '+':
+      case ',':
       case '-':
       case '/':
       case '*':
       case '(':
       case ')':
-        append(t, buffer[i]);
+        attach(t, buffer[i]);
         t = t->next;
         if (!INSTRING)
           while(isFormatChar(buffer[i+1]))
@@ -40,22 +42,60 @@ String remove_existing_formatting(char* buffer, long length){
         // the next char is not a formatting character
         break;
       default:
-        append(t, buffer[i]);
+        attach(t, buffer[i]);
         t = t->next;
         break;
     }
   }
+
+  // for the preceeding formatting characters
+  t = S->next;
+
+  while(t->next){
+    if (isFormatChar(t->next->ch)){
+      // skip all format chars if the other end has  , \{, ;... etc
+      // otherwise replace all format chars with one space
+      p = t->next;
+      while(isFormatChar(p->ch)){
+        q = p->next;
+        free(p);
+        p = q;
+      }
+      switch(p->ch){
+        case '}':
+        case '{':
+        case ';':
+        case '=':
+        case ',':
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+        case '(':
+        case ')':
+          t->next = p;
+          break;
+        default:
+          attach(t,' ');
+          t = t->next;
+          t->next = p;
+          break;
+      }
+    }
+    t = t->next;
+  }
+
   return S;
 }
 
-void append(String S, char c){
+void attach(Node t, char c){
   // while(S->next)
   //   S = S->next;
   
-  S->next = (Node)malloc(sizeof(struct strNode));
-  S = S->next;
-  S->ch = c;
-  S->next = NULL;
+  t->next = (Node)malloc(sizeof(struct strNode));
+  t = t->next;
+  t->ch = c;
+  t->next = NULL;
 }
 
 int isFormatChar(char c){
